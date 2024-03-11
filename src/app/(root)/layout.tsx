0a3ccdcaff949f67c0de64162e2bb4812c9748ec
LW Htet Aung <kiritokirikaya80@gmail.com>
2024-03-11 23:09:14 +0630

    Horizon - Adding Navbar with logo and User profile

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Components/Footer";
+import Header from "@/components/Components/Header";
+import { ChildrenType } from "@/types";
+
+export default function RootLayout({ children }: ChildrenType) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/Components/Header.tsx
@@ -0,0 +1,32 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b ">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            alt="Event Horizon logo"
+            src="/assets/Logo.svg"
+            width={40}
+            height={40}
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
