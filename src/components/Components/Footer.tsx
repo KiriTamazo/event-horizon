b5f13809cce9625908151ffe8256dcac080b33a6
LW Htet Aung <kiritokirikaya80@gmail.com>
2024-03-13 23:00:11 +0630

    Horizon - Add Footer

---
+++ src/components/Components/Footer.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            width={128}
+            height={38}
+            src="/assets/images/logo.svg"
+            alt="logo"
+          />
+        </Link>
+        <p>{year} Event Horizon. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
