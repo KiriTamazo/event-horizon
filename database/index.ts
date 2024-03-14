dec78a70129d18d14ec7a87bdd4c2d2369917603
LW Htet Aung <kiritokirikaya80@gmail.com>
2024-03-14 23:44:07 +0630

    Horizon - Added mongo set up and model

---
+++ database/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+let cached = (global as any).mongoose || { conn: null, promise: null }
+
+export const connectToDatabase = async() =>{
+    if(cached?.conn) return cached.conn
+    if(!MONGODB_URI) throw new Error("MONGODB_URI is missing")
+    const opts = {
+        dbName:'event_horizon',
+        bufferCommands: false,
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        // bufferMaxEntries: 0,
+        // useFindAndModify: true,
+        // useCreateIndex: true
+      }
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, opts)
+    cached.conn = await cached.promise
+
+      return cached.conn
+} 
