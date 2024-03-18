48dc24df7d24c94875565209e8da8d35afc55eef
LW Htet Aung <kiritokirikaya80@gmail.com>
2024-03-19 00:14:21 +0630

    Horizon - Event Crate Form setup and animation added

--- src/components/Components/EventForm.tsx
+++ src/components/Components/EventForm.tsx
@@ -1,13 +1,68 @@
 "use client";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { eventFormSchema } from "@/lib/validator";
+import { eventDefaultValues } from "@/data";
 
-import { auth } from "@clerk/nextjs";
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
 
 type EventFormProps = {
   userId: string;
   type: "Create" | "Update";
 };
+const initialValue = eventDefaultValues;
 
 const EventForm = ({ userId, type }: EventFormProps) => {
-  return <div>EventForm</div>;
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValue,
+  });
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5"
+        >
+          <div className="flex flex-col gap-5 md:flex-row"></div>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Event Title"
+                    className="input-field"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 export default EventForm;
