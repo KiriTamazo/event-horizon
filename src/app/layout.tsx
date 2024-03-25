53630d9ca2ae45c82730f7977966e3cf3d88e15f
LW Htet Aung <kiritokirikaya80@gmail.com>
2024-03-25 23:32:50 +0630

    Horizon - added some component and changed tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ChildrenType } from "@/types";
 import NextTopLoader from "nextjs-toploader";
+import "@uploadthing/react/styles.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
